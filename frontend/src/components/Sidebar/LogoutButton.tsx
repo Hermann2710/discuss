bb6e3d0ec4aff5b943f5cba994b5a37b3751be38
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2024-12-12 16:24:22 +0100

    Socket.Io

--- frontend/src/components/Sidebar/LogoutButton.tsx
+++ frontend/src/components/Sidebar/LogoutButton.tsx
@@ -4,7 +4,7 @@ import useLogout from "../../hooks/useLogout";
 export default function LogoutButton() {
   const { loading, logout } = useLogout();
   return (
-    <div className="mt-auto">
+    <div className="mt-auto flex flex-col justify-between">
       {!loading ? (
         <BiLogOut
           className="w-6 h-6 mt-4 text-white cursor-pointer"
@@ -13,6 +13,12 @@ export default function LogoutButton() {
       ) : (
         <span className="loading loading-spinner"></span>
       )}
+
+      <div className="avatar">
+        <div className="w-12">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
     </div>
   );
 }
